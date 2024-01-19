0b48af36aded977eefbd3743b00cd7b420d8a310
kew0611841021 <pangponii125@gmail.com>
2024-01-19 15:44:26 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use LDAP\Result;
+use Psy\CodeCleaner\ReturnTypePass;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $result = ['name'=> 'index','payload' => Product::all()];
+        return $result;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // Select * From Products Where id = 1
+        $payload = Product::find($id);
+        return ['name' =>'show','payload'=> $payload];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
